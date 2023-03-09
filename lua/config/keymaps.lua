710eda6945a07a5b65c943372a7f912ea8b2d8d3
crochee <13522570308@139.com>
2023-03-09 20:55:48 +0800

    [Refactor] fix hop

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -115,7 +115,7 @@ map("n", "<leader>c", ":bd<CR>", opt)
 --   "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true })<cr>"
 --   , {})
 -- map('n', 't', "<cmd>HopWord<cr>", {})
-map('n', '<leader>fh', ':HopChar1<CR>', opt)
+map('n', '<leader>fw', ':HopChar1<CR>', opt)
 
 ------------------------------- Telescope  文件搜索 -------------------------
 map("n", "<leader>e", ":Telescope find_files<CR>", opt)
