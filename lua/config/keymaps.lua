e49c69488149d0cb0d5eca4fdb855441f282e33a
crochee <licf137@chinaunicom.cn>
2023-03-09 17:37:19 +0800

    [Feature] add gitsigns

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -232,4 +232,29 @@ map("n", "<leader>dl", ":lua require'dap'.step_into()<CR>", opt)
 -- Pop-ups
 map("n", "<leader>dh", ":lua require'dapui'.eval()<CR>", opt)
 
+pluginKeys.mapgit = function(mapbuf)
+  -- Navigation
+  mapbuf('n', ']c', "&diff ? ']c' : '<cmd>Gitsigns next_hunk<CR>'", { expr = true })
+  mapbuf('n', '[c', "&diff ? '[c' : '<cmd>Gitsigns prev_hunk<CR>'", { expr = true })
+
+  -- Actions
+  mapbuf('n', '<leader>hs', ':Gitsigns stage_hunk<CR>')
+  mapbuf('v', '<leader>hs', ':Gitsigns stage_hunk<CR>')
+  mapbuf('n', '<leader>hr', ':Gitsigns reset_hunk<CR>')
+  mapbuf('v', '<leader>hr', ':Gitsigns reset_hunk<CR>')
+  mapbuf('n', '<leader>hS', '<cmd>Gitsigns stage_buffer<CR>')
+  mapbuf('n', '<leader>hu', '<cmd>Gitsigns undo_stage_hunk<CR>')
+  mapbuf('n', '<leader>hR', '<cmd>Gitsigns reset_buffer<CR>')
+  mapbuf('n', '<leader>hp', '<cmd>Gitsigns preview_hunk<CR>')
+  mapbuf('n', '<leader>hb', '<cmd>lua require"gitsigns".blame_line{full=true}<CR>')
+  mapbuf('n', '<leader>tb', '<cmd>Gitsigns toggle_current_line_blame<CR>')
+  mapbuf('n', '<leader>hd', '<cmd>Gitsigns diffthis<CR>')
+  mapbuf('n', '<leader>hD', '<cmd>lua require"gitsigns".diffthis("~")<CR>')
+  mapbuf('n', '<leader>td', '<cmd>gitsigns toggle_deleted<cr>')
+
+  -- Text object
+  mapbuf('o', 'ih', ':<C-U>Gitsigns select_hunk<CR>')
+  mapbuf('x', 'ih', ':<C-U>Gitsigns select_hunk<CR>')
+end
+
 return pluginKeys
